c4b0d242d74debd398535e82a5eff8a4f2f88250
Youcef Kadri <youcef.kadri@kraken.tech>
2026-04-04 12:46:43 +1300

    Bump version: 0.7.3 → 0.7.4

--- src/mapper/__init__.py
+++ src/mapper/__init__.py
@@ -18,7 +18,7 @@ from mapper.graph import Neo4jConnection
 from mapper.graph_loader import GraphLoader
 from mapper.name_resolver import NameResolver, UnresolvedName
 
-__version__ = "0.7.3"
+__version__ = "0.7.4"
 
 __all__ = [
     # Version
